15af221e43a13bd6a51aac5e3aaab89aa7140ad5
dteo827 <dteo827@gmail.com>
2015-02-28 15:07:40 -0500

    Update hardscript.sh

--- hardscript.sh
+++ hardscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# CentOS Configuration and Updater version 1.0
-# I freaking hate Centos 'nuff said
+# CentOS Configuration and Updater version 2.0
+# I hate Centos.
 # This script is intended for use in CentOS Linux Installations
 # Thanks to Pashapasta for the script template, check out the Kali version at https://github.com/PashaPasta/KaliUpdater/blob/master/KaliConfigAndUpdate.sh
 # Please contact dteo827@gmail.com with bugs or feature requests
@@ -31,15 +31,9 @@ fail2baninstalled = n
 function questions() {
 read -p "Do you want to add Google's and Level3's Public DNS to the resolv.conf file? [y/n]" answerGoogleDNS
 read -p "Do you want to turn off root login, Ipv6, keep boot as read only,and ignore ICMP broadcast requests and prevent XSS attacks? [y/n]" answerWegettinghard
-read -p "Do you want to install updates to CentOS Linux now? [y/n] " answerUpdate
-read -p "Do you want to install Bastille [y/n]" answerBastille
-read -p "Do you want to install Fail2ban [y/n]" answerFail2ban
-read -p "Do you want to install Curl [y/n]" answerCurl
-read -p "Do you want to setup OpenVAS? IF THIS IS A SERVER INSTALL SELECT NO!!! (Note: You will be prompted to enter a password for the OpenVAS admin user, this process may take up to an hour) [y/n] " answerOpenVAS
-read -p "Do you want to update Nikto's definitions? [y/n] " answerNikto
-read -p "Do you want to install PHP5? [y/n] " answerPhp
-read -p "Do you want to install Mysql? [y/n]" answerMysql
-read -p "Do you want to download (not install) Leopard Flower [y/n]" answerLeopardFlower
+read -p "Do you want to install security updates to CentOS Linux now? [y/n] " answerSecUpdate
+read -p "Do you want to install all updates to CentOS Linux now? [y/n] " answerUpdate
+read -p "Do you want to install Lynis [y/n]" answerLynis
 }
 
 # Flags!!!!
@@ -53,32 +47,14 @@ read -p "Are you sure you want to install all packages and configure everything 
 if [[ $answerWarning = y ]] ; then
 answerGoogleDNS=y
 answerWegettinghard=y
+answerSecUpdate=y
 answerUpdate=y
-answerBastille=y
-answerFail2ban=y
-answerOpenVAS=y
-answerCurl=y
-answerNikto=y
-answerPhp=y
-answerMysql=y
-answerLeopardFlower=y
+answerLynis=y
 else
 printf "Verify would you do and do not want done...."
 sleep 2
 questions
 fi
-
-elif [[ $1 = -s ]] ; then
-
-answerGoogleDNS=y
-answerWegettinghard=y
-answerUpdate=y
-answerBastille=y
-answerFail2ban=y
-answerCurl=y
-answerPhp=y
-answerMysql=y
-answerLeopardFlower=y
 
 elif [[ $1 = -h ]] ; then
 
@@ -101,9 +77,59 @@ fi
 if [[ $answerWegettinghard = y ]] ; then
 sudo echo  1 > /proc/sys/net/ipv4/icmp_echo_ignore_all
 sudo sed -i 's/.*PermitRootLogin.*yes/PermitRootLogin no/g' /etc/ssh/sshd_config
-sudo yum erase xinetd inetd tftp-server ypserv telnet-server rsh-server
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sysctl net.ipv6.conf.all.disable_ipv6=1
+yum install sudo perl ntp crontabs sendmail wget -y
+wget http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm
+wget http://rpms.famillecollet.com/enterprise/remi-release-5.rpm
+sudo rpm -Uvh remi-release-5*.rpm epel-release-5*.rpm
+yum repolist
+
+userdel shutdown
+userdel halt
+userdel games
+userdel operator
+userdel gopher
+awk -F: '($3 == "0") (print)' /etc/passwd
+yum erase xinetd inetd tftp-server ypserv telnet-server rsh-server
+
+#prevent running root cron tasks
+touch /etc/cron.allow
+chmod 600 /etc/cron.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/cron.deny
+touch /etc/at.allow
+chmod 600 /etc/at.allow
+awk -F: '{print $1}' /etc/passwd | grep -v root > /etc/at.deny
+
+#Narrowing rights
+chmod 700 /root
+chmod 700 /var/log/audit
+chmod 740 /etc/rc.d/init.d/iptables
+chmod 740 /sbin/iptables
+chmod -R 700 /etc/skel
+chmod 600 /etc/rsyslog.conf
+chmod 640 /etc/security/access.conf
+chmod 600 /etc/syctrl.conf
+
+#ignore ping broadcasts
+cat << 'EOF' >> /etc/sysctl.conf
+net.ipv4.icmp_echo_ignore_broadcasts = 1
+net.ipv4.icmp_echo_ignore_bogus_error_responses = 1
+net.ipv4.conf.all.send_redirects = 0
+net.ipv4.conf.default.send_redirects = 0
+net.ipv4.conf.all.accepts_redirects = 0
+net.ipv4.conf.all.secure_redirects = 0
+net.ipv4.conf.default.accept_redirects = 0
+authconfig --passalgo=sha512 --update
+
+fi
+
+if [[ $answerSecUpdate = y ]] ; then
+
+printf "Downloading Security updates for CentOS..."
+yum -y install yum-plugin-security
+sudo yum install yum
+sudo --security update
+
 fi
 
 if [[ $answerUpdate = y ]] ; then
@@ -111,6 +137,7 @@ if [[ $answerUpdate = y ]] ; then
 printf "Updating CentOS, this stage may take about an hour to complete...Hope you have some time to burn...
 "
 su -c 'yum update'
+yum update -y
 sudo yum install sudo perl ntp crontabs sendmail mlocate wget -y
 wget http://fedora.mirror.nexicom.net/epe1/6/i386/epel-release-6-7.noarch.rpm
 sudo yum install epel-release-6-7.noarch.rpm
@@ -118,137 +145,12 @@ sudo yum repolist
 
 fi
 
-if [[ $answerBastille = y ]] ; then
-http://www.cipherdyne.com/psad/download/psad-2.2.3-1.x86_64.rpm
-rpm -Uvh psad-2.2.3-1.x86_64.rpm
-rpm -ivh Bastille-3.2.1-0.1.noarch.rpm
-rpm -ivh perl-Tk-a.b-c.i386.rpm
-sudo bastille -c
-bastilleinstalled = y
-fi
-
-if [[ $answerFail2ban = y ]] ; then
-rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-sudo yum install fail2ban
-fail2baninstalled = y
-fi
-
-if [[ $answerOpenVAS = y ]] ; then
-wget -q -O - http://www.atomicorp.com/installers/atomic |sh
-sudo yum upgrade
-sudo yum install openvas
-openvas-setup
-fi
-
-if [[ $answerCurl = y ]] ; then
-sudo yum install php-common
-sudo yum install php-curl
-fi
-
-if [[ $answerNikto = y ]] ; then
-#wget https://www.cirt.net/nikto/nikto-2.1.5.tar.bz2
-#tar -zxvf nikto-2.1.5.tar.bz2
-#chmod +x /nikto-2.1.5/nikto.pl
-wget http://www6.atomicorp.com/channels/atomic/centos/6/i386/RPMS/atomic-release-1.0-18.el6.art.noarch.rpm
-rpm -Uvh atomic-release-1.0-18.el6.art.noarch.rpm
-sudo yum install nikto
-"
-fi
-
-if [[ $answerPhp = y ]] ; then
-sudo yum install php
-sed -i 's/memory_limit = 16M/memory_limit = 8M' /etc/php5/apache/php.ini
-fi
-
-if [[ $answerLeopardFlower = y ]] ; then
-wget http://iweb.dl.sourceforge.net/project/leopardflower/Source/lpfw-0.4-src.zip
-fi
-
-if [[ $answerMysql = y ]] ; then
-sudo yum install mysql-server
-sudo /sbin/service mysqld start
-fi
-
-# Not sure about this part
-# If OpenVAS was installed, check for error file, if present, print alert
-
-function filecheck () {
-file="/root/Desktop/openvas-info.txt"
-
-if [ -f "$file" ] ; then
-printf "Check /root/Desktop/openvas-info.txt for errors and recommendations
-"
-fi
-}
-if [[ $answerOpenVAS = y ]] ; then
-
-file="/root/Desktop/openvas-info.txt"
-
-filecheck
-printf "Note: OpenVAS user name is [admin]
-"
-sleep 3
-fi
-
-function pause () {
-read -p "$*"
-}
-
-if [[ $bastilleinstalled = y ]] ; then
-read -p "Do you want to configure Bastille? [y/n]" answerConfigBastille
-fi
-
-if [[ answerConfigBastille = y ]] ;
-printf"Here we go...."
-printf"
-
-#################################
-#This is what you need to select#
-#################################
-
-#File permissions module: Yes (suid)
-#Disable SUID for mount/umount: Yes
-#Disable SUID on ping: Yes
-#Disable clear-text r-protocols that use IP-based authentication? Yes
-#Enforce password aging? No (situation dependent, I have no users accessing my machines except me, and I only allow ssh keys)
-#Default umask: Yes
-#Umask: 077
-#Disable root login on tty’s 1-6: No
-#Password protect GRUB prompt: No (situation dependent, I’m on a VPS and would like to get support in case I need it)
-#Password protect su mode: Yes
-#default-deny on tcp-wrappers and xinetd? No
-#Ensure telnet doesn’t run? Yes
-#Ensure FTP does not run? Yes
-#Display authorized use message? No (situation dependent, if you had other users, Yes)
-#Put limits on system resource usage? Yes
-#Restrict console access to group of users? Yes (then choose root)
-#Add additional logging? Yes
-#Setup remote logging if you have a remote log host, I don’t so I answered No
-#Setup process accounting? Yes
-#Disable acpid? Yes
-#Deactivate nfs + samba? Yes (situation dependent)
-#Stop sendmail from running in daemon mode? No (I have this firewalled off, so I’m not concerned)
-#Deactivate apache? Yes
-#Disable printing? Yes
-#TMPDIR/TMP scripts? No (if a multi-user system, yes)
-#Packet filtering script? No (we configured the firewall previously)
-#Finished? YES! & reboot"
-
-fi
-
-if [[ fail2baninstalled = y ]] ; then
-read -p "Do you want to configure fail2ban? [y/n]" answerConfigfail2ban
-fi
-
-if [[ answerConfigfail2ban = y ]] ; then
-sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-sudo sed -i '/s/enabled  = true/enabled = false/g'/etc/fail2ban/jail.local
-sudo sed -i '/s/600/360000/g' /etc/fail2ban/jail.local
-printf"Restarting Fail2ban..."
-sudo service fail2ban restart
-
+if [[ $answerLynis = y ]]; then
+wget -q -O https://cisofy.com/files/lynis-1.6.4.tar.gz
+tar - xjf lynis-1.6.4.tar.gz
 
 
 pause '
 Press [Enter] key to exit...
 '
+fi
